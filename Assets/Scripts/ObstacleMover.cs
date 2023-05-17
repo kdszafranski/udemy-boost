bda28fdbd2ac9413c7166b27904a0291e825b91f
Kris Szafranski <kris@thoughtshelter.com>
2023-05-17 09:19:40 -0500

    completed obstacle mover logic with movement based on sin wave

--- Assets/Scripts/ObstacleMover.cs
+++ Assets/Scripts/ObstacleMover.cs
@@ -5,19 +5,35 @@ using UnityEngine;
 public class ObstacleMover : MonoBehaviour
 {
     Vector3 startingPosition;
-    [SerializeField] Vector3 movementVector;
-    [SerializeField] [Range(0,1)] float movementFactor;
+    float movementFactor; // length of repeating pattern, larger is slower
+    
+    [SerializeField] Vector3 movementVector; // location to move to
+    [SerializeField] float period = 2f; // length of repeating pattern, larger is slower
 
-    // Start is called before the first frame update
+
     void Start()
     {
         startingPosition = transform.position;    
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if(period != 0) {
+            CalculateMovement();
+        }
+    }
+
+    void CalculateMovement() {
+        float cycles = Time.time / period;  // continually growing over time
+
+        const float TAU = Mathf.PI * 2;     // constant 6.283
+        float rawSinWave = Mathf.Sin(cycles * TAU); // going from -1 to 1
+        
+        // recalculated to go from 0 to 1 so its cleaner
+        // +1 changes range from 0 - 2 then divide by 2 to get 0 - 1
+        movementFactor =  (rawSinWave + 1f) / 2f; 
+
         Vector3 offset = movementVector * movementFactor;
-        transform.position = startingPosition + offset;
+        transform.position = startingPosition + offset; 
     }
 }
